16a45588ec5125017d18a246d3a44ad27dfc0047
Christopher Hilton <kool1160@gmail.com>
2026-04-20 12:14:28 -0400

    Add meeting type enum

---
+++ enums/meeting-type.ts
@@ -0,0 +1,12 @@
+export type MeetingType =
+  | 'department'
+  | 'crossFunctional'
+  | 'executive'
+  | 'review';
+
+export const MEETING_TYPES: MeetingType[] = [
+  'department',
+  'crossFunctional',
+  'executive',
+  'review',
+];
