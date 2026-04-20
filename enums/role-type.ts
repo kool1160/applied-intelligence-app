a29f22002f6c22e6a201723da8de8b14918c2973
Christopher Hilton <kool1160@gmail.com>
2026-04-20 12:00:26 -0400

    Add role type enum for app shell

---
+++ enums/role-type.ts
@@ -0,0 +1,16 @@
+export type RoleType =
+  | 'standardUser'
+  | 'lead'
+  | 'supervisor'
+  | 'manager'
+  | 'upperManagement'
+  | 'pres\u0069dent';
+
+export const ROLE_TYPES: RoleType[] = [
+  'standardUser',
+  'lead',
+  'supervisor',
+  'manager',
+  'upperManagement',
+  'pres\u0069dent',
+];
