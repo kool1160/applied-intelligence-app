77d9ac056e8ec3a06442ad8b8ff8fc2b766a3624
Christopher Hilton <kool1160@gmail.com>
2026-04-20 18:07:50 -0400

    Update my-work-items-model.ts

--- models/my-work-items-model.ts
+++ models/my-work-items-model.ts
@@ -1,1 +1,18 @@
+import type { AckState } from '../enums/ack-state';
+import type { LinkedObjectType } from '../enums/linked-object-type';
+import type { SeverityLevel } from '../enums/severity-level';
+import type { QuickAction } from './helpers/quick-action';
 
+export type MyWorkItemModel = {
+  itemId: string;
+  itemType: string;
+  title: string;
+  linkedObjectRef: string;
+  linkedObjectType: LinkedObjectType;
+  status?: string;
+  severity?: SeverityLevel;
+  ackRequired?: boolean;
+  ackState?: AckState;
+  timestamp: string;
+  quickActions?: QuickAction[];
+};
