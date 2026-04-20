b7e1edf16fd9cf217d57fdf073f004b13cbb6b0f
Christopher Hilton <kool1160@gmail.com>
2026-04-20 12:08:22 -0400

    Add notification type enum

---
+++ enums/notification-type.ts
@@ -0,0 +1,18 @@
+export type NotificationType =
+  | 'notice'
+  | 'alert'
+  | 'reminder'
+  | 'escalation'
+  | 'meeting'
+  | 'handoff'
+  | 'b\u0072oadcast';
+
+export const NOTIFICATION_TYPES: NotificationType[] = [
+  'notice',
+  'alert',
+  'reminder',
+  'escalation',
+  'meeting',
+  'handoff',
+  'b\u0072oadcast',
+];
