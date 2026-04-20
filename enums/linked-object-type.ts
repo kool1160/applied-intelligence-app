21553bf38c1f88a9e112b2ea945e5bea7660d055
Christopher Hilton <kool1160@gmail.com>
2026-04-20 12:52:47 -0400

    Add type enum file

---
+++ enums/linked-object-type.ts
@@ -0,0 +1,18 @@
+export type LinkedObjectType =
+  | 'report'
+  | 'handoff'
+  | 'meeting'
+  | 'thread'
+  | 'part'
+  | 'broadcast'
+  | 'escalation';
+
+export const LINKED_OBJECT_TYPES: LinkedObjectType[] = [
+  'report',
+  'handoff',
+  'meeting',
+  'thread',
+  'part',
+  'broadcast',
+  'escalation',
+];
